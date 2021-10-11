02cf8363a79621466621c816547d442c6e1257cf
Arkadiy Vyaznikov <varkasha@yandex-team.ru>
2021-10-11 19:37:25 +0300

    fix tests

--- tests/move_extractor_test.py
+++ tests/move_extractor_test.py
@@ -1,5 +1,7 @@
 import unittest
-from request_test import RequestTest
+
+from alice_scripts import Request
+
 from move_extractor import MoveExtractor
 
 
@@ -7,76 +9,66 @@ class TestMoveExtractor(unittest.TestCase):
     def setUp(self):
         self.move_extractor = MoveExtractor()
 
+    def command(self, command):
+        return Request(
+            {'request': {'command': command},
+             'session': {
+                 'session_id': '440518e7-36a2-411a-9a91-e88ce94a8e5c',
+                 'user_id': 'beafdead'
+             }})
+
+    def extract_move_test(self, command_text, expected_move):
+        request = self.command(command_text)
+        actual_move = self.move_extractor.extract_move(request)
+        if expected_move is None:
+            self.assertIsNone(actual_move)
+        else:
+            self.assertEqual(expected_move, actual_move)
+
     def test_move_without_piece(self):
-        request = RequestTest('бэ 5')
-        move = self.move_extractor.extract_move(request)
-        self.assertEqual(move, 'b5')
+        self.extract_move_test('бэ 5', 'b5')
 
     def test_move_without_space(self):
-        request = RequestTest('e5')
-        move = self.move_extractor.extract_move(request)
-        self.assertEqual(move, 'e5')
+        self.extract_move_test('e5', 'e5')
 
     def test_move_with_piece(self):
-        request = RequestTest('Конь эф 3')
-        move = self.move_extractor.extract_move(request)
-        self.assertEqual(move, 'Nf3')
+        self.extract_move_test('Конь эф 3', 'Nf3')
 
     def test_move_with_from(self):
-        request = RequestTest('Конь эф 3 же 5')
-        move = self.move_extractor.extract_move(request)
-        self.assertEqual(move, 'Nf3g5')
+        self.extract_move_test('Конь эф 3 же 5', 'Nf3g5')
 
     def test_move_with_2_pieces(self):
         # it returns first found piece by order in setting list
-        request = RequestTest('Слон Конь а 3 цэ 5')
-        move = self.move_extractor.extract_move(request)
-        self.assertEqual(move, 'Na3c5')
+        self.extract_move_test('Слон Конь а 3 цэ 5', 'Na3c5')
 
     def test_empty_input(self):
-        request = RequestTest('')
-        move = self.move_extractor.extract_move(request)
-        self.assertIsNone(move)
+        self.extract_move_test('', None)
 
     def test_no_move(self):
-        request = RequestTest('Fill something here')
-        move = self.move_extractor.extract_move(request)
-        self.assertIsNone(move)
+        self.extract_move_test('Fill something here', None)
 
     def test_move_with_excess_text(self):
-        request = RequestTest('Конь гыгыгыгы д 6')
-        move = self.move_extractor.extract_move(request)
-        self.assertEqual(move, 'Nd6')
+        self.extract_move_test('Конь гыгыгыгы д 6', 'Nd6')
 
     def test_move_without_digits(self):
-        request = RequestTest('Конь гыгыгыгы эф дэ')
-        move = self.move_extractor.extract_move(request)
-        self.assertIsNone(move)
+        self.extract_move_test('Конь гыгыгыгы эф дэ', None)
 
     def test_move_with_big_digits(self):
-        request = RequestTest('Ладья ф 9')
-        move = self.move_extractor.extract_move(request)
-        self.assertIsNone(move)
+        self.extract_move_test('Ладья ф 9', None)
 
     def test_short_casting(self):
-        request = RequestTest('Короткая рокировка')
-        move = self.move_extractor.extract_move(request)
-        self.assertEqual(move, '0-0')
+        self.extract_move_test('Короткая рокировка', 'O-O')
 
     def test_two_zeros(self):
-        request = RequestTest('Два нуля')
-        move = self.move_extractor.extract_move(request)
-        self.assertEqual(move, '0-0')
+        self.extract_move_test('Два нуля', 'O-O')
+        self.extract_move_test('00', 'O-O')
 
     def test_three_zeros(self):
-        request = RequestTest('Три нуля')
-        move = self.move_extractor.extract_move(request)
-        self.assertEqual(move, '0-0-0')
+        self.extract_move_test('Три нуля', 'O-O-O')
+        self.extract_move_test('000', 'O-O-O')
 
     def test_long_casting(self):
-        request = RequestTest('Длинная рокировка')
-        move = self.move_extractor.extract_move(request)
-        self.assertEqual(move, '0-0-0')
+        self.extract_move_test('Длинная рокировка', 'O-O-O')
 
 
 if __name__ == '__main__':
