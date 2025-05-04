5c0b20ea8da6362596d72648913b207372e58064
axtrace <4077607+axtrace@users.noreply.github.com>
2025-05-05 00:21:59 +0300

    fix: add package configuration for Yandex Cloud Functions

--- setup.py
+++ setup.py
@@ -7,10 +7,14 @@ def get_requirements(file_name):
 
 
 setuptools.setup(
-    name="alice_blind_chess",
+    name="alisa_blind_chess",
     version="0.1.0",
     description="Alice Skill - Blind Chess",
     author_email="S0finma@ya.ru",  # change 0 to o
     install_requires=get_requirements("requirements.txt"),
     packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_data={
+        '': ['*.py', '*.txt', '*.md'],
+    },
 )
